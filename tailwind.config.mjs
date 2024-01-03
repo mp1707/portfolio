060a8ea907b5775e1108e57593cf6bfac2576dd1
mp1707 <marco.preuss1707@gmail.com>
2024-01-03 17:11:27 +0100

    retro design

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,20 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        blink: "blink 1s linear infinite",
+      },
+      keyframes: {
+        blink: {
+          "0%, 100%": { opacity: 1 },
+          "50%": { opacity: 0 },
+        },
+      },
+    },
   },
   plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["light", "dark", "cupcake"],
+  },
 };
